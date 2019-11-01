79168d8451243cd1d5f40b2dc39655e388a55e2d
renanrodriguesmr <renan.rodriguesmr@gmail.com>
2019-10-31 22:34:42 -0300

    ResponseMessagesClass created

---
+++ src/br/com/IME/CasinoRoulette/ResponseMessages.java
@@ -0,0 +1,13 @@
+package br.com.IME.CasinoRoulette;
+
+/**
+ * Class that convert object to String messages to response client
+ * Pattern of return messages of roulette result : roulette/{value}
+ * Pattern of return messages of bet: bet/{0/1}
+ * Pattern of return messages of player info: player/info/{name}/{coins}
+ * Pattern of return messages of player login/logout: player/{login/logout}/{name}/coins
+ *  -
+ */
+public class ResponseMessages {
+
+}
